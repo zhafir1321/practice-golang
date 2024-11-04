782aff750141e10eae72b4646408f5f57f136432
Zhafir <zahfir1000@hotmail.com>
2024-11-04 10:13:47 +0700

    hello world

---
+++ hello-world/hello-world.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+// output "Hello, World!"
+
+// go run hello-world.go => run the code without compiling it
+
+// go build hello-world.go => compile the code and create an executable file
+
+// ./hello-world => run the executable file
